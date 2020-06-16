2017bdda1d1c472227e03b8135944f267ee80633
robin.leckey <robin.leckey@gmail.com>
2020-06-16 16:37:32 -0600

    third commit of demo program on web app for the goats

--- demo1.go
+++ demo1.go
@@ -7,7 +7,6 @@ import "net/http"
 import "html/template"
 import "fmt"
 
-
 type GoatsPage struct{
   Title string
   Goats string
@@ -21,13 +20,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request){
 func goatsHandler(w http.ResponseWriter, r *http.Request){
   p := GoatsPage{Title: "Cute Goats in Colorado Springs", Goats: "Here are some goats"}
   t, err := template.ParseFiles("index2.gohtml")
+
   fmt.Println(err)
   t.Execute(w, p)
 }
 //Beginning of main
 func main(){
+  //Handles all requests to the web root
   http.HandleFunc("/", indexHandler)
   http.HandleFunc("/goat/", goatsHandler)
+  //Tells the server to isten on the TCP network address
   http.ListenAndServe(":8080", nil)
 }
 //End of main
